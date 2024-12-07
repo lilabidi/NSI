53fcdde5bf33805f88614650b16c2081a9a0bdee
Eddy Labidi <144728990+lilabidi@users.noreply.github.com>
2024-12-07 14:36:57 +0000

    square test

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import arrr
 from pyscript import document
 
+def calculate_square(event):
+    input_value = document.querySelector("#inputNumber").value
+    result_div = document.querySelector("#result")
+    
+    if input_value:
+        try:
+            number = float(input_value)
+            square = number ** 2
+            result_div.innerText = f"The square of {number} is {square}."
+        except ValueError:
+            result_div.innerText = "Please enter a valid number."
+    else:
+        result_div.innerText = "Please enter a number."
 
-def translate_english(event):
-    input_text = document.querySelector("#english")
-    english = input_text.value
-    output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
