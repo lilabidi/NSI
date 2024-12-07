3283bd4138a661780d3f65756e15f074790868ec
Eddy Labidi <144728990+lilabidi@users.noreply.github.com>
2024-12-07 14:51:00 +0000

    dernier test

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 from pyscript import document
 
-def calculate_square(event):
-    input_value = document.querySelector("#inputNumber").value
-    result_div = document.querySelector("#result")
-    
-    if input_value:
-        try:
-            number = float(input_value)
-            square = number ** 2
-            result_div.innerText = f"The square of {number} is {square}."
-        except ValueError:
-            result_div.innerText = "Please enter a valid number."
+# Tables pour afficher les éléments
+S = document.getElementById("stack")
+Q = document.getElementById("queue")
+e = document.getElementById("e")
+
+# Fonctions de manipulation des données
+def empty(S):
+    return S.childElementCount == 0
+
+def insert(S, i, x):
+    S.insertCell(i).append(x)
+
+def push(S, x):
+    insert(S, -1, x)
+
+def queue(S, x):
+    insert(S, 0, x)
+
+def stacktop(S):
+    return int(S.children[S.childElementCount - 1].textContent)
+
+def pop(S):
+    x = stacktop(S)
+    S.deleteCell(S.childElementCount - 1)
+    return x
+
+def small_step():
+    if empty(e):
+        push(e, pop(Q))
+    elif not empty(S) and stacktop(e) < stacktop(S):
+        queue(Q, pop(S))
     else:
-        result_div.innerText = "Please enter a number."
+        push(S, pop(e))
+
+def start(lst):
+    while not empty(S): pop(S)
+    while not empty(Q): pop(Q)
+    for x in lst:
+        push(Q, x)
